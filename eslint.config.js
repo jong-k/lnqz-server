89e3af6c2b2c9c343ff5b38cc39cdab98079a9cd
jong-k <fully.charged07@gmail.com>
2025-10-15 12:38:09 +0900

    feat: eslint 플러그인 탑재 추가

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,13 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import js from "@eslint/js";
+import nodePlugin from "eslint-plugin-n";
+import pluginPromise from "eslint-plugin-promise";
 
 export default defineConfig([
   { ignores: ["dist/**"] },
+  nodePlugin.configs["flat/recommended-script"],
+  pluginPromise.configs["flat/recommended"],
   js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
