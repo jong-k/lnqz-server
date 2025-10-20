d7eb273c403ba502837245bcb82aa9ab509030ba
jong-k <fully.charged07@gmail.com>
2025-10-20 15:34:46 +0900

    feat: convert PORT to number before starting the server

--- src/app.ts
+++ src/app.ts
@@ -90,7 +90,7 @@ const start = async () => {
   try {
     const fastify = await buildServer();
     const port = fastify.config.PORT;
-    await fastify.listen({ port });
+    await fastify.listen({ port: Number(port) });
   } catch (err) {
     console.error(err);
     // eslint-disable-next-line n/no-process-exit
